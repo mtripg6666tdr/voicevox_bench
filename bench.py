3788823067620fa27cd03cc3294d8c487cf8e323
user.email <0kq@0kqnet.work>
2022-11-22 22:20:32 +0900

    結果表示の改善

--- bench.py
+++ bench.py
@@ -2,6 +2,7 @@ import requests
 import random
 import json
 from time import perf_counter
+import argparse
 
 def synthesis(text:str,address="127.0.0.1",port=50021,speaker=0,pitch=0.0,speed=1.0):
   """音声生成
@@ -14,7 +15,7 @@ def synthesis(text:str,address="127.0.0.1",port=50021,speaker=0,pitch=0.0,speed=
       speed : スピード
   
   Returns:
-      
+      生成時間(秒), レイテンシ(秒)
   """
   address = "http://"+address
   query_payload = {"text": text,"speaker":speaker}
@@ -26,11 +27,15 @@ def synthesis(text:str,address="127.0.0.1",port=50021,speaker=0,pitch=0.0,speed=
   query_data["speedScale"] = speed
   query_data["pitchScale"] = pitch
   before = perf_counter()
+  requests.get(f"{address}:{port}/version")
+  after = perf_counter()
+  latency = after - before
+  before = perf_counter()
   resp = requests.post(f"{address}:{port}/synthesis",params=synth_payload,data=json.dumps(query_data))
   if not resp.status_code == 200:
     raise ConnectionError("Status code: %d" % resp.status_code)
   after = perf_counter()
-  return after - before
+  return after - before - latency , latency
 
 def gen_text(count:int):
   return "".join([chr(random.randint(ord("あ"),ord("ん"))) for i in range(count)])
@@ -51,15 +56,38 @@ def bench(length:int,count=10,address="127.0.0.1",port=50021):
   tmp = 0
   for i in range(count):
     text = gen_text(length)
-    elapsed_time = synthesis(text,address=address,port=port)
+    elapsed_time,latency = synthesis(text,address=address,port=port)
     tmp += elapsed_time
-    print(i+1,elapsed_time)
+    print(i+1,elapsed_time,latency)
     result = round(tmp / count,4)
   return result
 
-score_10 = bench(length=10,address="127.0.0.1")
-score_50 = bench(length=50,address="127.0.0.1")
-score_100 = bench(length=100,address="127.0.0.1")
-print("10:",score_10)
-print("50:",score_50)
-print("100:",score_100)
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-s",help="VOICEVOX API Server Address",default="127.0.0.1")
+  parser.add_argument("-p",help="VOICEVOX API Server Port",default=50021)
+  args = parser.parse_args()
+  score_10 = bench(length=10,address=args.s,port=args.p)
+  score_50 = bench(length=50,address=args.s,port=args.p)
+  score_100 = bench(length=100,address=args.s,port=args.p)
+  score_avg = round((score_10 + score_50 + score_100) / 3,4)
+  resp = requests.get(f"http://{args.s}:{args.p}/version")
+  info_engine = resp.text.replace("\"","")
+  resp = requests.get(f"http://{args.s}:{args.p}/supported_devices")
+  info_devices = resp.json()
+  if info_devices["cuda"]:
+    info_device = "CUDA"
+  elif info_devices["dml"]:
+    info_device = "DirectML"
+  else:
+    info_device = "CPU"
+  
+  print("=========== Info ===========")
+  print(" Engine:",info_engine)
+  print(" Device:",info_device)
+  print("========== Result ==========")
+  print(" 10: ",score_10)
+  print(" 50: ",score_50)
+  print(" 100:",score_100)
+  print(" Avg:",score_avg)
+  print("============================")
