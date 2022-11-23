e9ca89bc929b5e2219ff216debf1020a80780302
user.email <0kq@0kqnet.work>
2022-11-23 18:24:13 +0900

    遅延の計算を削除

--- bench.py
+++ bench.py
@@ -26,16 +26,16 @@ def synthesis(text:str,address="127.0.0.1",port=50021,speaker=0,pitch=0.0,speed=
   synth_payload = {"speaker": speaker}
   query_data["speedScale"] = speed
   query_data["pitchScale"] = pitch
-  before = perf_counter()
-  requests.get(f"{address}:{port}/version")
-  after = perf_counter()
-  latency = after - before
+  #before = perf_counter()
+  #requests.get(f"{address}:{port}/version")
+  #after = perf_counter()
+  #latency = after - before
   before = perf_counter()
   resp = requests.post(f"{address}:{port}/synthesis",params=synth_payload,data=json.dumps(query_data))
   if not resp.status_code == 200:
     raise ConnectionError("Status code: %d" % resp.status_code)
   after = perf_counter()
-  return after - before - latency , latency
+  return after - before #- latency , latency
 
 def gen_text(count:int):
   return "".join([chr(random.randint(ord("あ"),ord("ん"))) for i in range(count)])
@@ -56,10 +56,11 @@ def bench(length:int,count=10,address="127.0.0.1",port=50021,quiet=False):
   tmp = 0
   for i in range(count):
     text = gen_text(length)
-    elapsed_time,latency = synthesis(text,address=address,port=port)
+    #elapsed_time,latency = synthesis(text,address=address,port=port)
+    elapsed_time = synthesis(text,address=address,port=port)
     tmp += elapsed_time
     if not quiet:
-      print(i+1,"time:",elapsed_time,"latency:",latency)
+      print(i+1,"time:",elapsed_time)
     result = round(tmp / count,4)
   return result
 
